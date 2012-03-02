8edde4a4ad54aca6e90800e9456eabd809d464a2
Patrick <patrick@bittorrent.com>
2012-03-01 17:21:17 -0800

    updating app messaging

--- nudgs.js
+++ nudgs.js
@@ -132,7 +132,7 @@ $(function(){
 	window.btapp.bind('add:bt:sendappmsg', function() {
 		var func = function() {
 			var msg = {
-				image: 'https://github.com/favicon.ico',
+				image: 'http://a2.twimg.com/profile_images/1158280382/nudge_icon.png',
 				link: 'http://pwmckenna.github.com/nud.gs/',
 				src: 'nud.gs'
 			};
